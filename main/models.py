a9156fc24a8823454727cf94a6200d8244dffeb6
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-03 16:42:39 +0700

    Push Tugas 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)  
+    price = models.IntegerField() 
+    description = models.TextField()  
+    thumbnail = models.URLField()  
+    category = models.CharField(max_length=50)  
+    is_featured = models.BooleanField(default=False)  
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_name(request):
+    person = {
+        'name' :'Antonius Daniel',
+        'npm' : '2406496012',
+        'class' : 'PBP E'
+    }
+    return render(request, "main.html", person)
