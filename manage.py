42116dabc042f57e48bd7b1d9089b4ba03e6fb5e
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-09 23:01:31 +0700

    Mengubah nama project menjadi BluR Sport

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LBM_SHOP.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BluR_Sport.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
