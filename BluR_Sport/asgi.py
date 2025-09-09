7631328638275a5bea848fb04976347c76ea1697
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-09 23:23:45 +0700

    Complete Tugas 2

--- BluR_Sport/asgi.py
+++ BluR_Sport/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LBM_SHOP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BluR_Sport.settings')
 
 application = get_asgi_application()
