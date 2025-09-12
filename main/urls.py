e12fcd1a373285f2fec5d01056ba8faa5fb5471d
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-13 01:05:44 +0700

    Complete Tugas 3

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.show_name, name='show_name'),
-
+    path('create_product/', views.create_product, name="create_product"),
+    path('product_list/', views.product_list,name="product_list"),
+    path('products/<int:pk>', views.product_detail,name="product_detail"),
+    path('xml/',views.show_xml,name="show_xml"),
+    path('json/', views.show_json,name="show_json"),
+    path('xml/<str:news_id>/',views.show_xml_by_id,name="show_xml_by_id"),
+    path('json/<str:news_id>/',views.show_json_by_id,name="show_json_by_id"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Product
+from .forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_name(request):
@@ -9,3 +13,52 @@ def show_name(request):
         'class' : 'PBP E'
     }
     return render(request, "main.html", person)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('product_list')
+
+    context = {"form" : form}
+    return render(request,"form.html",context)
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products' : products}
+    return render(request,'product_list.html',context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "detail.html", {"product": product})
+
+def show_xml(request):
+    products = Product.objects.all()
+    xml_data = serializers.serialize("xml" , products)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_xml_by_id(request, news_id):
+   try:
+       product = Product.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", product)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json(request):
+    products = Product.objects.all()
+    json_data = serializers.serialize('json', products)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_json_by_id(request, news_id):
+   try:
+       product = Product.objects.filter(pk=news_id)
+       json_data = serializers.serialize("json", product)
+       return HttpResponse(json_data, content_type="application/json")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
+
+
+
+
