1f3136b3518a21e709b8c28dd475a58b7606c8b3
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-13 01:10:58 +0700

    Complete Tugas 3

--- BluR_Sport/settings.py
+++ BluR_Sport/settings.py
@@ -147,3 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://antonius-daniel-footballshop.pbp.cs.ui.ac.id"
+]
