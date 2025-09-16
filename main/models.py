ebd50790e127eec2937d58381b82038e2979385c
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-16 22:06:16 +0700

    Final Tugas 3

--- main/models.py
+++ main/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+    ('jersey', 'Jersey'),
+    ('sepatu', 'Sepatu'),
+    ('peralatan', 'Peralatan Olahraga'),
+    ('aksesoris', 'Aksesoris'),
+    ('bola', 'Bola'),
+    ('lainnya', 'Lainnya'),
+]
+
     name = models.CharField(max_length=50)  
     price = models.IntegerField() 
     description = models.TextField()  
     thumbnail = models.URLField()  
-    category = models.CharField(max_length=50)  
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES,default='update')  
     is_featured = models.BooleanField(default=False)  
 
     def __str__(self):
