69df96f4a1bef706e0f74e6bb7a7f48fa086aeaf
antoniusdaniel06 <antonius.daniel03@gmail.com>
2025-09-21 22:00:22 +0700

    Complete Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -9,7 +10,7 @@ class Product(models.Model):
     ('bola', 'Bola'),
     ('lainnya', 'Lainnya'),
 ]
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=50)  
     price = models.IntegerField() 
     description = models.TextField()  
@@ -19,4 +20,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class employee(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    persona = models.TextField()
     
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
     path('',views.show_name, name='show_name'),
     path('create_product/', views.create_product, name="create_product"),
@@ -10,4 +12,9 @@ urlpatterns = [
     path('json/', views.show_json,name="show_json"),
     path('xml/<str:news_id>/',views.show_xml_by_id,name="show_xml_by_id"),
     path('json/<str:news_id>/',views.show_json_by_id,name="show_json_by_id"),
+    path('register/', views.register,name="register"),
+    path('login/', views.login_user,name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('employee/',views.add_employee, name='employee'),
+    
 ]
